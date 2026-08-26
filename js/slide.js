77a1c5454c1076a8f452391b3f9495331b65dad4
guihgalegodev <guigalego96@gmail.com>
2026-08-26 12:44:55 -0300

    first commit

---
+++ js/slide.js
@@ -0,0 +1,22 @@
+// export default class Slide {
+//   constructor(element) {
+//     this.element = document.querySelector(element);
+
+//     this.changeColor = this.changeColor.bind(this);
+//   }
+
+//   changeColor() {
+//     this.element.style.color = "blue";
+//   }
+
+//   addSlideEvents() {
+//     this.element.addEventListener("click", this.changeColor);
+//   }
+
+//   init() {
+//     if (this.element) {
+//       this.addSlideEvents();
+//     }
+//     return this;
+//   }
+// }
